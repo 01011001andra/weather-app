9de2d0d4ce15d253036b82d015eb081c675b7f52
Yandra Muslim <yandrayt1@gmail.com>
2025-03-22 06:20:33 +0700

    update

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -90,7 +90,7 @@ const HomePage = () => {
           </div>
         </div>
         <div className="size-96">
-          <img src="https://github.com/01011001andra/weather-app/actions/workflows/deploy.yml/badge.svg" />
+          <img src="https://github.com/01011001andra/weather-app/actions/workflows/deploy.yml/badge.svg?cachebuster=$(date +%s)" />
         </div>
 
         {/* Daily alert */}
@@ -139,4 +139,4 @@ const HomePage = () => {
   );
 };
 
-// export default HomePage;
+export default HomePage;
